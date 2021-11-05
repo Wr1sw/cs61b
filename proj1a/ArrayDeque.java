928d50452d714ae93b820e7fef4a189a81876427
wr1sw <840265545@qq.com>
2021-11-05 20:45:18 +0800

    completed proj1a

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -14,29 +14,38 @@ public class ArrayDeque<T> {
         size = 0;
     }
 
-//    public void resize(int newSize) {
-//        T[] temp = (T[]) new Objects[newSize];
-//        System.arraycopy(items, 0, temp, 0, newSize);
-//        items = temp;
-//    }
 
+    /** Returns true if deque is empty, false otherwise. **/
     public boolean isEmpty() {
         return nextFirst == nextLast;
     }
+
+
+    public boolean isFull() {
+        return size == capacity;
+    }
+
+    /**  Adds an item of type T to the front of the deque. **/
     public void addFirst (T item) {
-//        if ()
+        if (isFull()) {
+            resize((int) (capacity * 1.2));
+        }
         items[nextFirst] = item;
         nextFirst = (nextFirst-1+capacity)%capacity;
+        size = size + 1;
     }
 
+    /** Adds an item of type T to the back of the deque. **/
     public void addLast (T item) {
-//        if(nextLast+1 > capacity) {
-//            resize((int) (capacity * 1.2));
-//        }
+        if (isFull()) {
+            resize((int) (capacity * 1.2));
+        }
         items[nextLast] = item;
         nextLast = (nextLast + 1 + capacity) % capacity;
+        size = size + 1;
     }
 
+    /** Removes and returns the item at the front of the deque. If no such item exists, returns null. */
     public T removeFirst() {
         if(isEmpty()) {
             return null;
@@ -46,10 +55,14 @@ public class ArrayDeque<T> {
         items[k] = null;
 
         nextFirst = (nextFirst+1+capacity)%capacity;
-//       if usage-rate < 25%
+        size = size -1;
+        if (isLowUseageRate()) {
+            resize((int) (capacity*0.4));
+        }
         return res;
     }
 
+    /** Removes and returns the item at the back of the deque. If no such item exists, returns null. */
     public T removeLast() {
         if (isEmpty()) {
             return null;
@@ -58,8 +71,106 @@ public class ArrayDeque<T> {
         T res = items[k];
         items[k] = null;
         nextLast = (nextLast-1+capacity)%capacity;
-        //       if usage-rate < 25%
+        size = size -1;
+        if (isLowUseageRate()) {
+            resize((int) (capacity*0.4));
+        }
+
         return res;
     }
+
+    /** Returns the number of items in the deque. **/
+    public int size() {
+        return size;
+    }
+
+    /** Gets the item at the given index, where 0 is the front, 1 is the next item, and so forth. If no such item exists, returns null. Must not alter the deque! */
+    public T get(int index) {
+        if (index < 0 || index >= size || isEmpty()) {
+            return null;
+        }
+        int k = 0;
+        if (nextFirst < nextLast) {
+            k = nextFirst+1+index;
+        }else {
+            if (nextFirst+index+1 < capacity) {
+                k = (nextFirst+index+1);
+            }else {
+                k = (nextFirst+1+index+capacity)%capacity;
+            }
+        }
+        return items[k];
+    }
+
+    public void resize(int newSize) {
+        T[] newArray = (T[]) new Object[newSize];
+
+        //1
+        if (nextFirst < nextLast) {
+            for (int i = nextFirst+1, j = 0; i <= nextLast-1;i++, j++) {
+                newArray[j] = items[i];
+            }
+        }
+        if (nextFirst > nextLast) {
+            int j = 1;
+            for(int i = nextLast+1;i < capacity-nextFirst-1;i++, j++) {
+                newArray[j] = items[i];
+            }
+            for(int i = 0; i < nextLast; i++, j++) {
+                newArray[j] = items[i];
+            }
+        }
+        nextFirst = 0;
+        nextLast = size + 1;
+        items = newArray;
+        capacity = newSize;
+    }
+
+    /** For arrays of length 16 or more, your usage factor should always be at least 25% **/
+    public boolean isLowUseageRate () {
+        if (capacity >= 16 &&  ((double)size / capacity) < 0.25) {
+            return true;
+        }
+        return false;
+    }
+
+    /** Prints the items in the deque from first to last, separated by a space. **/
+    public void printDeque() {
+        if (nextFirst < nextLast) {
+            for (int i = nextFirst + 1; i < nextLast;i++) {
+                if (i == nextLast -1) {
+                    System.out.println(items[i]);
+                }
+                System.out.println(items[i]+" ");
+            }
+        }
+        else {
+            for (int i = nextFirst + 1; i < capacity;i++) {
+                System.out.println(items[i]+" ");
+            }
+            for (int i = 0;i < nextLast; i++) {
+                if (i == nextLast-1) {
+                    System.out.println(items[i]);
+                }
+                System.out.println(items[i] + " ");
+            }
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
